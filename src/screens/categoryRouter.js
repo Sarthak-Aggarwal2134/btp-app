d0da107ce79fae28ebfedd55222d73599a69e53d
Sarthak Aggarwal <sarthak.aggarwal@students.iiit.ac.in>
2023-03-17 12:06:09 +0530

    bargraph and star

--- src/screens/categoryRouter.js
+++ src/screens/categoryRouter.js
@@ -7,6 +7,7 @@ import Lesson from "./lesson";
 import Results from "./results";
 import Results_present from "./results_present";
 import Video_page from "./video_page";
+import Bar_graph from "./results_graph";
 const CategoryStack = createStackNavigator();
 
 export default function CategoryRouter({ category }) {
@@ -23,6 +24,9 @@ export default function CategoryRouter({ category }) {
       </CategoryStack.Screen> */}
       <CategoryStack.Screen name="Results_present">
         {(props) => <Results_present {...props} />}
+      </CategoryStack.Screen>
+      <CategoryStack.Screen name="Bar_graph">
+        {(props) => <Bar_graph {...props} />}
       </CategoryStack.Screen>
       <CategoryStack.Screen name="Video_page">
         {(props) => <Video_page {...props} />}
